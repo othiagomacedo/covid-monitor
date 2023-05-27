cc4bfac13c02fbf11086f73ad2b99ea4f3db87da
Chirana-Dev <engsw.thiago@gmail.com>
2023-05-27 03:17:38 -0300

    [START] iniciando projeto covid-monitor

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+      <button @click="changeMessage">clique aqui</button>
+    </div>
+  </template>
+  
+  <script>
+  import './styles/style-geral.css'
+  export default {
+    data() {
+      return {
+        message: "se clicar é guei"
+      };
+    },
+    methods: {
+      changeMessage() {
+        this.message = "ih, seu baitola!";
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  h1 {
+    color: var(--color-text-dark);
+  }
+  button {
+    margin-top: 50px;
+  }
+  </style>
+  
